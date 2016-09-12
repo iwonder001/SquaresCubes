68e815907da414ef91340257bf967bbb1fdb1de5
Kristen Carethers <kcarethers@gmail.com>
2016-09-11 21:35:25 -0400

    Initial Commit

---
+++ src/Application.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// download scanner class
+		//sysout number, square & cubes line (use \t to have program tab inbetween words)
+		//sysout 7=\t, 7=\t, 6=\t
+		
+		
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter an integer: ");
+		int num = in.nextInt();
+		
+		//int i;
+		
+		
+		
+		//get user input
+		
+
+	}
+
+}
